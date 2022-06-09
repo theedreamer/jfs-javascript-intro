7b70afd871de0b169ed5176c4e87d94ce82deeb0
theedreamer <104943395+theedreamer@users.noreply.github.com>
2022-06-09 14:52:02 -0500

    I have made sn edit to this file!

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let myNumber = 12
+ console.log(myNumber);
+if (myNumber === "Getu") {
+  console.log("This is not a number");
+} else if(myNumber * 3){
+  console.log("Gen");
+} else if (myNumber * 5) {
+  console.log("Buzz");
+} else if (myumber * 15){
+  console.log("GenBuzz");
+} else {
+  console.log("myNumber");
+}
+
+
+let fashionItem = "Jeans"
+switch (fashionItem){
+case "shoes":
+    console.log("shoes are $50.");
+break;
+case "jeans":
+   console.log("Jeans are $25.");
+break;
+case "hats":
+   console.log("Hats are $12.");
+break;
+case "socks":
+   console.log("socks are $2.");
+break;
+default:
+   console.log("Invalid item.")
+}
+console.log(Math.floor(Math.random()*100));
